fa8dbbf7fc31b7d14e67a5b2bf253fc836684ba6
lengushuru charles <charleslengushuru@gmail.com>
2023-04-01 21:51:22 +0300

    Fix linter error

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,5 @@
 const Categories = () => (
- <button type="button" className="btn btn-primary">Categories</button>
+  <button type="button" className="btn btn-primary">Categories</button>
 );
 
 export default Categories;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 const Home = () => (
   <div className="home">
     <h1>Welcome to Bookstore</h1>
-  <ul>
-    <li>bookname - author</li>
-  </ul>
-  <form>
-    <input type="text" placeholder="title" />
-    <input type="text" placeholder="author" />
-    <button type="submit">Add Book</button>
-  </form>
+    <ul>
+      <li>bookname - author</li>
+    </ul>
+    <form>
+      <input type="text" placeholder="title" />
+      <input type="text" placeholder="author" />
+      <button type="submit">Add Book</button>
+    </form>
   </div>
 );
 
