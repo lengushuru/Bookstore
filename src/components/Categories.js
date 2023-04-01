d083bf28efe4fc3726de96bf2f4687723aebd62b
lengushuru charles <charleslengushuru@gmail.com>
2023-04-01 21:29:04 +0300

    Add App components.

---
+++ src/components/Categories.js
@@ -0,0 +1,9 @@
+const Categories = () => (
+  <form>
+    <input type="text" placeholder="title" />
+    <input type="text" placeholder="author" />
+    <button type="submit">Add Book</button>
+  </form>
+);
+
+export default Categories;
