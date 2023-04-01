f0d6c8b53e4830952815aebced9a3cb7edec4320
lengushuru charles <charleslengushuru@gmail.com>
2023-04-01 21:42:23 +0300

    Updated Categories.js component.

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,5 @@
 const Categories = () => (
-  <form>
-    <input type="text" placeholder="title" />
-    <input type="text" placeholder="author" />
-    <button type="submit">Add Book</button>
-  </form>
+ <button type="button" className="btn btn-primary">Categories</button>
 );
 
 export default Categories;
