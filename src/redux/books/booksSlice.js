609e7bfff327561f0ffe7fa9a4473e2dd53c5157
lengushuru charles <charleslengushuru@gmail.com>
2023-04-05 15:22:50 +0300

    Add reducer for categories and books.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,23 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   books: [],
-  isLoading:false,
-}
+  isLoading: false,
+};
 
 const BooksSlice = createSlice({
-  name:'book',
+  name: 'book',
   initialState,
-  reducers:{
-    addBook:(state,action)=>{
-      state.books = [...state.books,action.payload]
+  reducers: {
+    addBook: (state, action) => {
+      // eslint-disable-next-line no-param-reassign
+      state.books = [...state.books, action.payload];
     },
-    removeBook:(state,action)=>{
-    state.books = state.books.filter((book)=>{return book.id !==action.payload})
-    }
-  }
-})
+    removeBook: (state, action) => {
+      // eslint-disable-next-line no-param-reassign
+      state.books = state.books.filter((book) => book.id !== action.payload);
+    },
+  },
+});
 
-export const {addBook,removeBook} = BooksSlice.actions
+export const { addBook, removeBook } = BooksSlice.actions;
 
-export default BooksSlice.reducer
+export default BooksSlice.reducer;
