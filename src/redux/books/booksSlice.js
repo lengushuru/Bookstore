64ed41fcd8654a96c9672f0eb49ad743bffa40ec
lengushuru charles <charleslengushuru@gmail.com>
2023-04-12 18:49:48 +0300

    Add extra reducers and api connection.

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -21,15 +21,12 @@ export const newBook = createAsyncThunk('book/addBook', async (book) => {
     fetchBooks();
     return book;
   } catch {
-    console.log('error adding new book');
+    return 'error adding new book';
   }
-
-  // return response.data;
 });
 
 export const deleteBook = createAsyncThunk('book/removeBook', async (id) => {
   await axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/tfn9nyLdsA3a9Lmk93em/books/${id}`);
-  console.log(id);
   return id;
 });
 
@@ -44,31 +41,36 @@ const BooksSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(fetchBooks.pending, (state) => {
-      state.isLoading = true;
-    });
-    builder.addCase(fetchBooks.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.books = [...state.books, ...action.payload];
-    });
-    builder.addCase(fetchBooks.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    });
+    builder.addCase(fetchBooks.pending, (state) => ({ ...state, isLoading: true }));
+    builder.addCase(fetchBooks.fulfilled, (state, action) => ({
+      ...state,
+      isLoading: true,
+      books: [...action.payload],
+    }));
+    builder.addCase(fetchBooks.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload,
+    }));
     builder.addCase(newBook.fulfilled, (state, { payload }) => {
-      state.isLoading = false;
-      state.books = [...state.books, payload];
+      const newstate = { ...payload };
+      return (
+
+        { ...state, books: [...state.books, newstate] });
     });
-    builder.addCase(newBook.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    });
+    builder.addCase(newBook.rejected, (state, action) => ({
+      ...state,
+      isLoading: false,
+      error: action.payload,
+    }));
     builder.addCase(deleteBook.fulfilled, (state, { payload }) => {
-      state.isLoading = false;
       const deletedBookId = payload;
-      state.books = state.books.filter((book) => book.Id !== deletedBookId);
+      return ({
+        ...state,
+        isLoading: false,
+        books: [...state.books.filter((book) => book.Id !== deletedBookId)],
+      });
     });
   },
 });
-
 export default BooksSlice.reducer;
