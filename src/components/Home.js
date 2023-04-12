e94614544f82ea0a7342b959535d2720d2d18093
lengushuru charles <charleslengushuru@gmail.com>
2023-04-12 18:49:03 +0300

    Remove commented codes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,20 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { nanoid } from '@reduxjs/toolkit';
-import { useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import Book from './Book';
 import { newBook, fetchBooks } from '../redux/books/booksSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
-
-  // const { books } = useSelector((state) => state.book);
-
   const books = useSelector((state) => state.book.books);
-  console.log(books);
-
-  // dispatch(newBook({
-  //   item_id: nanoid(), title: 'The Hobbit', author: 'J.R.R. Tolkien', category: '',
-  // }));
-  // dispatch(newBook({
-  //   item_id: nanoid(), title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', category: '',
-  // }));
-  // dispatch(newBook({
-  //   item_id: nanoid(), title: 'The Silmarillion', author: 'J.R.R. Tolkien', category: '',
-  // }));
-  // dispatch(newBook({
-  //   item_id: nanoid(), title: 'The Chronicles of Narnia', author: 'C.S. Lewis', category: '',
-  // }));
-
   const handleSubmit = (e) => {
-    const item_id = nanoid();
+    dispatch(fetchBooks());
+    const itemId = uuidv4();
     const categories = '';
     e.preventDefault();
     const title = e.target[0].value.trim();
     const author = e.target[1].value.trim();
     dispatch(newBook({
-      item_id, title, author, category: categories,
+      item_id: itemId, title, author, category: categories,
     }));
     e.target[0].value = '';
     e.target[1].value = '';
