3566156cc1bbd5d7ac105f8296817b02a1f79216
lengushuru charles <charleslengushuru@gmail.com>
2023-04-15 01:33:40 +0300

    Fix linter errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="navbar">
-    <p className="logo">Bookstore CMS</p>
+    <p className="logo">Bookstore_CMS</p>
     <div className="links">
-      <Link to="/" className="nav-books">BOOKS</Link>
+      <Link to="/" className="nav-book">BOOKS</Link>
       <Link to="/categories" className="nav-categories">CATEGORIES</Link>
     </div>
   </nav>
